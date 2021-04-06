8a3f8ff87bd1e369a0fc73fe9ee07110bd22dd07
dongzhi <895180729@qq.com>
2021-04-06 11:31:30 +0800

    datasource

--- dynamic-datasource/web/src/App.vue
+++ dynamic-datasource/web/src/App.vue
@@ -1,15 +1,44 @@
 <template>
   <div id="app">
-    <router-view/>
+    <layout/>
+    <side-menu />
+    <!-- 页面左侧二级菜单栏，和主体内容区域部分 -->
+    <el-main>
+      <router-view></router-view>
+    </el-main>
   </div>
 </template>
 
+<script>
+import Layout from "@/components/Layout";
+import SideMenu from "@/components/SideMenu";
+export default {
+  components: {
+    Layout,
+    SideMenu
+  }
+}
+</script>
+
 <style lang="less">
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
+  position: relative;
+
+  .el-main {
+    height: 100%;
+    padding: 80px 0 0 260px;
+    background-color: #eee;
+  }
 }
 </style>
---
+++ dynamic-datasource/web/src/components/SideMenu.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="SideMenu">
+
+    <!-- 一级菜单下面所拥有的二级菜单 -->
+    <el-aside>
+      <SideBar :items='items'></SideBar>
+    </el-aside>
+  </div>
+</template>
+
+<script>
+import SideBar from "@/components/SideBar";
+export default {
+  name: 'SideMenu',
+  data() {
+    return {
+      items: [
+        {
+          index: 'test1-1',
+          title: '二级菜单1-1'
+        },
+        {
+          index: 'test1-2',
+          title: '二级菜单1-2'
+        },
+        {
+          index: 'test1-3',
+          title: '二级菜单1-3'
+        },
+        {
+          index: 'test1-4',
+          title: '二级菜单1-4'
+        },
+        {
+          index: 'test1-5',
+          title: '二级菜单1-5'
+        }
+      ],
+    }
+  },
+  components: {
+    SideBar
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+
+    },
+  },
+  beforeDestroy() {
+
+  }
+}
+</script>
+
+<style scoped lang='less'>
+.SideMenu {
+  position: fixed;
+  height: 100%;
+}
+</style>
